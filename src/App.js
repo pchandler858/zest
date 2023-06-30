e3288df0efefb460203db1f0aa4012940576e099
Preston C <pchandler858@gmail.com>
2023-06-29 18:12:32 -0700

    front end work

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import Dashboard from './pages/Dashboard';
-
+import { ThemeContext, useMode } from "./theme";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./pages/global/Topbar";
+import Sidebar from "./pages/global/Sidebar";
+// import Dashboard from "./pages/dashboard";
+import Applications from "./pages/applications";
+// import Contacts from "./pages/contacts";
+// import Form from "./pages/form";
+import Calendar from "./pages/calendar";
+// import Bar from "./pages/bar";
+// import Line from "./pages/line";
+// import Pie from "./pages/pie";
 
 function App() {
+  const { colorMode, theme } = useMode();
   return (
-    <div>
-      {/* < Login /> */}
-      < Dashboard />
-      {/* < Applications />
-      < Todo />
-      < Projects />
-      < Calendar />
-      < Mylinks />
-      < Logout /> */}
-    </div>
+    <ThemeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              {/* <Route path="/" element={<Dashboard />} /> */}
+              <Route path="/applications" element={<Applications />} />
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              <Route path="/calendar" element={<Calendar />} />
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ThemeContext.Provider>
   );
 }
 
---
+++ src/pages/applications/index.jsx
@@ -0,0 +1,76 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme.js";
+import { seedData } from "../../data/seedData.js";
+import Header from "../../components/Header.jsx";
+
+const Applications = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1, // flex: 1 is the same as flex-grow: 1 and flex-shrink: 1, combined.
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+  ];
+
+  return (
+    <Box m="20px">
+      <Header
+        title="Applications"
+        subtitle="See all of your submitted applications"
+      />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTip: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+        }}
+      >
+        <DataGrid rows={seedData} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Applications;
