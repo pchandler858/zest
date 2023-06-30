45ce966a66da2e68477da028c4d69c32a8fd67de
Preston C <pchandler858@gmail.com>
2023-06-29 18:31:12 -0700

    more frontend work

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./pages/global/Topbar";
 import Sidebar from "./pages/global/Sidebar";
-// import Dashboard from "./pages/dashboard";
+import Dashboard from "./pages/dashboard";
 import Applications from "./pages/applications";
-// import Contacts from "./pages/contacts";
-// import Form from "./pages/form";
+import Contacts from "./pages/contacts";
+import Form from "./pages/form";
 import Calendar from "./pages/calendar";
-// import Bar from "./pages/bar";
-// import Line from "./pages/line";
-// import Pie from "./pages/pie";
+import Bar from "./pages/bar";
+import Line from "./pages/line";
+import Pie from "./pages/pie";
 
 function App() {
   const { colorMode, theme } = useMode();
@@ -23,14 +23,14 @@ function App() {
           <main className="content">
             <Topbar />
             <Routes>
-              {/* <Route path="/" element={<Dashboard />} /> */}
+              <Route path="/" element={<Dashboard />} />
               <Route path="/applications" element={<Applications />} />
-              {/* <Route path="/contacts" element={<Contacts />} /> */}
-              {/* <Route path="/form" element={<Form />} /> */}
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
-              {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/pie" element={<Pie />} /> */}
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/pie" element={<Pie />} />
             </Routes>
           </main>
         </div>
---
+++ src/pages/dashboard/index.jsx
@@ -0,0 +1,171 @@
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import CodeIcon from "@mui/icons-material/Code";
+import HandshakeIcon from "@mui/icons-material/Handshake";
+import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
+import Header from "../../components/Header";
+import LineChart from "../../components/LineChart";
+import BarChart from "../../components/BarChart";
+import StatBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
+
+const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+
+      {/* GRID & CHARTS */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+        {/* ROW 1 */}
+        <Box
+          gridColumn="span 4"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="45"
+            subtitle="Number of Applications"
+            progress="0.75"
+            increase="+14%"
+            icon={
+              <WorkOutlineIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 4"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="6"
+            subtitle="Number of Projects"
+            progress="0.50"
+            increase="+21%"
+            icon={
+              <CodeIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 4"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="1"
+            subtitle="Number of Interviews"
+            progress="0.10"
+            increase="+5%"
+            icon={
+              <HandshakeIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        {/* ROW 2 */}
+        <Box
+          gridColumn="span 8"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+        >
+          <Box
+            mt="25px"
+            p="0 30px"
+            display="flex "
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box>
+              <Typography
+                variant="h5"
+                fontWeight="600"
+                color={colors.grey[100]}
+              >
+                Statistics
+              </Typography>
+              <Typography
+                variant="h3"
+                fontWeight="bold"
+                color={colors.greenAccent[500]}
+              >
+                Put info here if we want
+              </Typography>
+            </Box>
+          </Box>
+          <Box height="250px" m="-20px 0 0 0">
+            <LineChart isDashboard={true} />
+          </Box>
+        </Box>
+        {/* ROW 3 */}
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          p="30px"
+        >
+          <Typography variant="h5" fontWeight="600">
+            Random Pie Chart
+          </Typography>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            mt="25px"
+          >
+            <ProgressCircle size="125" />
+            <Typography
+              variant="h5"
+              color={colors.greenAccent[500]}
+              sx={{ mt: "15px" }}
+            >
+              Random info here
+            </Typography>
+            <Typography>Sub random info here</Typography>
+          </Box>
+        </Box>
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+        >
+          <Typography
+            variant="h5"
+            fontWeight="600"
+            sx={{ padding: "30px 30px 0 30px" }}
+          >
+            Sales Quantity
+          </Typography>
+          <Box height="250px" mt="-20px">
+            <BarChart isDashboard={true} />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
