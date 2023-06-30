93d2789de2a66855c9e842c71659160dac24bb55
Chris H <122721974+CH3RNAND3Z@users.noreply.github.com>
2023-06-29 20:01:18 -0700

    Added Schema, Sign-up, and Sign-in

---
+++ src/models/User.js
@@ -0,0 +1,52 @@
+const {Schema, model} = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new Schema({
+    username: { 
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lastName:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+});
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+    };
+
+const User = model('User', userSchema);
+
+module.exports = User;
+
