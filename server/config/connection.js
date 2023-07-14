73f6b0486b797c0b48ac5b5762f6331cc3a28d01
Preston C <pchandler858@gmail.com>
2023-07-13 20:37:57 -0700

    test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/zest');
+const connectionString =
+  "mongodb+srv://pchandler858:Password12345678@zest.2mczm7x.mongodb.net/zest";
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log("Error connecting to the database: " + err));
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/zest");
 
 module.exports = mongoose.connection;
