1222f17e1c987c2cbbaa5a0aad3d612e30efdc26
Preston C <pchandler858@gmail.com>
2023-07-13 20:51:30 -0700

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,16 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const connectionString =
-  "mongodb+srv://pchandler858:Password12345678@zest.2mczm7x.mongodb.net/zest";
-
-mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Database connected successfully"))
-  .catch((err) => console.log("Error connecting to the database: " + err));
-
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/zest");
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/zest');
 
 module.exports = mongoose.connection;
