f035742cef8e012fbbde81689cbb3e6e65bc9582
Preston C <pchandler858@gmail.com>
2023-08-24 21:51:21 -0700

    added env and switched to mongodb cloud as primary

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-// test  db dd
 mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/zest");
 
 module.exports = mongoose.connection;
